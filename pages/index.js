e21187f285b1fc886f6b69194e55b959e6f8f898
Patrick Johan Cuentas Mariano <patrick_cuentas@hotmail.com>
2021-11-20 18:11:20 -0500

    Card Design Finished

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,87 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 font-outfit bg-neutral-very_dark_blue_main text-white">
       <Head>
-        <title>Create Next App</title>
+        <title>NFT-preview-card</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+      <main className="bg-neutral-very_dark_blue_card max-w-sm p-8 space-y-6 rounded-xl">
+        <section id="image">
+          <Image
+            src="/image-equilibrium.jpg"
+            width="250px"
+            height="250px"
+            layout="responsive"
+            className="rounded-xl"
+          />
+        </section>
+        <section id="content">
+          <h1 className="text-xl">Equilibrium #3429</h1>
+          <p className="text-primary-soft_blue mt-4">
+            Our Equilibrium collection promotes balance and calm.
+          </p>
+          <div className="flex justify-between mt-4">
+            <p className="flex items-center text-primary-cyan space-x-2">
+              <Image
+                src="/icon-ethereum.svg"
+                width="11px"
+                height="18px"
+                fill="responsive"
+              />
+              <span>0.041 ETH</span>
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+            <p className="flex items-center text-primary-soft_blue space-x-2">
+              <Image
+                src="/icon-clock.svg"
+                width="17px"
+                height="17px"
+                fill="responsive"
+              />
+              <span>3 days left</span>
             </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+          </div>
+        </section>
+        <hr />
+        <section id="user">
+          <div className="flex items-center space-x-5">
+            <div id="border-image" className="rounded-full">
+              <Image
+                src="/image-avatar.png"
+                width="48px"
+                height="48px"
+                fill="responsive"
+                id="borde-xd"
+              />
+            </div>
+            <p>
+              <span className="text-primary-soft_blue">Creation of </span>Jules
+              Wyvern
             </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          </div>
+        </section>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <style jsx>{`
+        #border-image {
+          width: 48px;
+          height: 48px;
+          border: 1.5px solid white;
+        }
+        hr {
+          border: 0.5px solid hsl(215, 32%, 27%);
+          height: 1px;
+          width: 100%;
+        }
+      `}</style>
     </div>
-  )
+  );
+  {
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          soft_blue: "hsl(215, 51%, 70%)",
+          cyan: "hsl(178, 100%, 50%)",
+        },
+        neutral: {
+          very_dark_blue_main: "hsl(217, 54%, 11%)",
+          very_dark_blue_card: "hsl(216, 50%, 16%)",
+          very_dark_blue_line: "hsl(215, 32%, 27%)",
+        },
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
